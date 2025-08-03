42aeb23848e276f7c26f1e7ac8983397453f9222
gracemarrone <88406889+gracemarrone@users.noreply.github.com>
2025-08-03 12:49:47 -0300

    first commit gulp
    Work with gulp for the first time

---
+++ source/scripts/aritmetica.js
@@ -0,0 +1,5 @@
+// nesse arquivo quero importar o conteúdo de outro arquivo js, através do require fazemos a importação do conteúdo da função somar
+
+const somar = require('./sum');
+
+console.log("Somar: ", somar(10,2));
---
+++ source/scripts/sum.js
@@ -0,0 +1,9 @@
+function somar(a, b){
+    return a + b;
+}
+
+module.exports = somar; 
+
+// DENTRO DO ARQUIVO QUE QUEREMOS IMPORTAR USAMOS O MÓDULO EXPORTS E O NOME DA FUNÇÃO CONFORME O EXEMPLO ABAIXO
+// module.exports = somar; 
+// E IMPORTAMOS NO ARQUIVO DE DESTINO
